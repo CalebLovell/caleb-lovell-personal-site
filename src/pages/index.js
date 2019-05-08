137569a52b4447db012a1723c379ce25c6999292
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-05-07 19:06:26 -0600

    test auto-deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Caleb Lovell</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>This is an auto-deploy test.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
