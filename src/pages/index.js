a4e70033f2029ebb2cb6b713fe00004eeb64d0e4
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-06-12 14:22:35 -0600

    portfolio setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -11,8 +10,11 @@ const IndexPage = () => (
     <h1>Caleb Lovell</h1>
     <p>This is an auto-deploy test.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className='image__holder'>
+      <img
+        src="https://avatars0.githubusercontent.com/u/39709791?s=460&v=4"
+        alt="My beautiful face should have been displayed here"
+      />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
